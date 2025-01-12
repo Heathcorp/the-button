ee797e98ebfe2f66340027fdd2c052a83a9a2f08
Heath Manning <heath@manningf.com>
2025-01-12 12:01:30 +1100

    Add unused spinner code.

--- web/src/components/Counter.tsx
+++ web/src/components/Counter.tsx
@@ -5,6 +5,7 @@ import {
   Match,
   Show,
   Switch,
+  onCleanup,
 } from "solid-js";
 
 import "./components.css";
@@ -29,9 +30,11 @@ const Counter: Component<{
             props.loadingStatus === "UPLOADING"
           }
         >
+          {/* <Spinner /> */}
           {"~"}
           {props.count}
         </Match>
+
         <Match when={props.loadingStatus === "LOADING"}>{"Loading..."}</Match>
         <Match when={props.loadingStatus === "ERROR"}>{"Error!"}</Match>
       </Switch>
@@ -41,8 +44,21 @@ const Counter: Component<{
 
 export default Counter;
 
-// const SPINNER_LOOP_CHARACTERS = ["-", "\\", "|", "/"];
-// const spinner: Component<{ spinning: boolean }> = (props) => {
-//   const i = createEffect(() => );
-//   return <>{SPINNER_LOOP_CHARACTERS[i()]}</>;
-// };
+const SPINNER_LOOP_CHARACTERS = ["-", "\\", "|", "/"];
+// const SPINNER_LOOP_CHARACTERS = ["-", "+", "|", "+"];
+// const SPINNER_LOOP_CHARACTERS = [">", "v", "<", "^"];
+const Spinner: Component<{}> = (props) => {
+  const [i, setI] = createSignal(0);
+
+  const interval = setInterval(() => {
+    setI(
+      (prev) =>
+        (prev - 1 + SPINNER_LOOP_CHARACTERS.length) %
+        SPINNER_LOOP_CHARACTERS.length
+    );
+  }, 75);
+
+  onCleanup(() => clearInterval(interval));
+
+  return <>{SPINNER_LOOP_CHARACTERS[i()]}</>;
+};
